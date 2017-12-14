9af770cd3f508e692dfeb0f54b69dc8494253365
Dan Staines <dstaines@ebi.ac.uk>
2017-12-14 16:49:05 +0000

    pushed port into config object

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;        
+var port = process.env.PORT || cfg.port;        
 
 var router = express.Router();              
 
